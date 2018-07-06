de19ff8f7fd178cba82c53564ab69a252ca03806
kangxiongwei <kangxiongwei@163.com>
2018-07-06 22:30:57 +0800

    配置中心代码调整

--- kxw-spring-cloud-config-client/src/main/java/org/kxw/springcloud/config/server/CloudConfigClientApplication.java
+++ kxw-spring-cloud-config-client/src/main/java/org/kxw/springcloud/config/server/CloudConfigClientApplication.java
@@ -2,13 +2,13 @@ package org.kxw.springcloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Create by kangxiongwei on 2018/7/5 14:20.
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {
