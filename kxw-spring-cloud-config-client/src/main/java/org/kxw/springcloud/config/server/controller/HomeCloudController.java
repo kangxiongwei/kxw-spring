2edf51d698525363f2ea4719a59e765b0fc4fb07
亢雄伟 <kangxiongwei@meituan.com>
2018-07-06 18:32:56 +0800

    刷新配置中心文件

--- kxw-spring-cloud-config-client/src/main/java/org/kxw/springcloud/config/server/controller/HomeCloudController.java
+++ kxw-spring-cloud-config-client/src/main/java/org/kxw/springcloud/config/server/controller/HomeCloudController.java
@@ -1,22 +1,25 @@
 package org.kxw.springcloud.config.server.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 这边的@RefreshScope注解不能少，否则即使调用/refresh，配置也不会刷新
  * Create by kangxiongwei on 2018/7/5 19:15.
  */
 @RestController
+@RefreshScope
 public class HomeCloudController {
 
-    @Value("${foo}")
-    private String port;
+    @Value("${profile}")
+    private String profile;
 
-    @RequestMapping("/hi")
+    @RequestMapping("/hello")
     public String home(@RequestParam String name) {
-        return "Hello " + name + ", I am foo " + port;
+        return "Hello " + name + ", I am profile " + profile;
     }
 
 }
